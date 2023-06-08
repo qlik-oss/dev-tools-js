adb006cf8d835a48f41c5fda0fe6ef737d971c7e
Anders Nilsson <nilssonanders79@gmail.com>
2023-06-08 15:04:11 +0200

    fix(eslint-config): svelte-js fix

---
+++ .changeset/metal-cows-camp.md
@@ -0,0 +1,5 @@
+---
+"@qlik/eslint-config": patch
+---
+
+Fix svelte-js config
--- packages/eslint-config/svelte-js.js
+++ packages/eslint-config/svelte-js.js
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: ["./index", "./overrides/sveltets"],
+  extends: ["./index", "./overrides/sveltejs"],
 };
