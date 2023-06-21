54d0e4e66c7496d4504baf3ad461d076a4c0cf50
Anders Nilsson <nilssonanders79@gmail.com>
2023-06-21 09:19:09 +0200

    fix(eslint-config): react-svelte config fix

---
+++ .changeset/tidy-queens-learn.md
@@ -0,0 +1,5 @@
+---
+"@qlik/eslint-config": patch
+---
+
+Fix react-svelte config to reference the correct file
--- packages/eslint-config/react-svelte.js
+++ packages/eslint-config/react-svelte.js
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: ["./index", "./overrides/react", "./overrides/svelte"],
+  extends: ["./index", "./overrides/react", "./overrides/sveltets"],
 };
