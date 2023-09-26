f3accdd1735b6488cc56aa18ca7f0ab1dc13c588
Anders Nilsson <nilssonanders79@gmail.com>
2023-09-26 13:20:20 +0200

    fix(eslint-config): react-svelte config for .svelte files (#96)

---
+++ test/test-react-svelte/src/SvelteComponent.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let name: string;
+</script>
+
+<main>
+  <section class="my-pink-section">Props: {`{ name: "${name}" }`}</section>
+</main>
+
+<style>
+  .my-pink-section {
+    color: pink;
+  }
+</style>
