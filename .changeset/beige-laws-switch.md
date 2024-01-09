35141b90451c9850d399d1b1f913ca97d5ec3473
Anders Nilsson <nilssonanders79@gmail.com>
2024-01-09 14:27:11 +0100

    feat: turn off  rule in playwright config

---
+++ .changeset/beige-laws-switch.md
@@ -0,0 +1,5 @@
+---
+"@qlik/eslint-config": minor
+---
+
+Turn off `playwright/no-networkidle` rule for playwright config
--- packages/eslint-config/playwright.js
+++ packages/eslint-config/playwright.js
@@ -5,6 +5,7 @@ module.exports = {
       extends: ["plugin:playwright/playwright-test"],
       rules: {
         "import/no-extraneous-dependencies": "off",
+        "playwright/no-networkidle": "off",
       },
     },
   ],
