3845c5c61494de20055be234b9d6ea1c2bf00a7a
Egill Sveinbjörnsson <egill.sveinbjornsson@qlik.com>
2024-11-21 14:28:16 +0100

    fix: get svelte rules to work

--- packages/eslint-config/src/configs/svelte.js
+++ packages/eslint-config/src/configs/svelte.js
@@ -1,17 +1,46 @@
 // @ts-check
 import eslintPluginSvelte from "eslint-plugin-svelte";
+import svelteParser from "svelte-eslint-parser";
+import tsEslint from "typescript-eslint";
 import { mergeConfigs } from "../utils/config.js";
+import rules from "./rules/svelte.js";
 
 /**
  * @type {import("../types/index.js").ESLintFlatConfig}
  * config for Svelte https://github.com/sveltejs/eslint-plugin-svelte
  */
-const playwright = mergeConfigs(...eslintPluginSvelte.configs["flat/recommended"], {
+const svelte = mergeConfigs(...eslintPluginSvelte.configs["flat/recommended"], {
   name: "svelte",
+  files: ["**/*.svelte"],
+  plugins: {
+    svelte: eslintPluginSvelte,
+  },
+  languageOptions: {
+    parser: svelteParser,
+    parserOptions: {
+      parser: tsEslint.parser,
+      extraFileExtensions: [".svelte"],
+    },
+  },
   rules: {
+    ...rules,
     // modify rules from eslint-plugin-svelte here
+
+    // Conflicting rules
+    // https://github.com/sveltejs/eslint-plugin-svelte3/blob/master/OTHER_PLUGINS.md
+    "import-x/first": "off",
+    "import-x/no-duplicates": "off",
+    "import-x/no-mutable-exports": "off",
+    "import-x/no-unresolved ": "off",
+    "import-x/prefer-default-export": "off",
+
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
   },
 });
 
-export default [playwright];
-export { playwright };
+export default [svelte];
+export { svelte };
