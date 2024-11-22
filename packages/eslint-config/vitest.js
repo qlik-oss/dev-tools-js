96a663337dd9ad75da2fecb6fe36079a73ae41be
Egill Sveinbjörnsson <egill.sveinbjornsson@qlik.com>
2024-11-22 14:24:14 +0100

    chore: update deps, switch to new vitest package

--- packages/eslint-config/vitest.js
+++ packages/eslint-config/vitest.js
@@ -2,8 +2,8 @@ module.exports = {
   overrides: [
     {
       files: ["**/__tests__/**/*.{js,ts,jsx,tsx}", "**/*.{spec,test}.{js,ts,jsx,tsx}"],
-      plugins: ["vitest", "testing-library"],
-      extends: ["plugin:vitest/recommended"],
+      plugins: ["@vitest", "testing-library"],
+      extends: ["plugin:@vitest/legacy-recommended"],
       rules: {
         "import/no-extraneous-dependencies": "off",
         "@typescript-eslint/unbound-method": "off",
