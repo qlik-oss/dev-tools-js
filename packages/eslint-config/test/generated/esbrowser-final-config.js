118f62ffede70471d0c15d421249b13539590272
Egill Sveinbjörnsson <egill.sveinbjornsson@qlik.com>
2025-03-17 13:54:45 +0100

    update snapshots

--- packages/eslint-config/test/generated/esbrowser-final-config.js
+++ packages/eslint-config/test/generated/esbrowser-final-config.js
@@ -3387,9 +3387,9 @@ export default [
         "parser": {
           "meta": {
             "name": "typescript-eslint/parser",
-            "version": "8.26.0"
+            "version": "8.26.1"
           },
-          "version": "8.26.0"
+          "version": "8.26.1"
         },
         "projectService": true,
         "warnOnUnsupportedTypeScriptVersion": false
