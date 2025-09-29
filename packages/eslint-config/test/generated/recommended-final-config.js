69f0c6da4bc0a1cbae2c1734533b7f15316809b1
PipelineBot <57624568+svc-pipeline@users.noreply.github.com>
2025-09-29 13:11:59 +0200

    chore: update npm dependencies (#291)
    Co-authored-by: Anders Nilsson <nilssonanders79@gmail.com>

--- packages/eslint-config/test/generated/recommended-final-config.js
+++ packages/eslint-config/test/generated/recommended-final-config.js
@@ -3467,9 +3467,9 @@ export default [
         "parser": {
           "meta": {
             "name": "typescript-eslint/parser",
-            "version": "8.44.0"
+            "version": "8.44.1"
           },
-          "version": "8.44.0"
+          "version": "8.44.1"
         },
         "projectService": true,
         "warnOnUnsupportedTypeScriptVersion": false
@@ -5977,9 +5977,9 @@ export default [
         "parser": {
           "meta": {
             "name": "typescript-eslint/parser",
-            "version": "8.44.0"
+            "version": "8.44.1"
           },
-          "version": "8.44.0"
+          "version": "8.44.1"
         },
         "projectService": true,
         "warnOnUnsupportedTypeScriptVersion": false
