34a9d392b271823bd8aea86a9b82942fbb016617
svc-pipeline <svc_pipeline@qlik.com>
2026-01-16 07:45:51 +0000

    chore: update npm dependencies

--- packages/eslint-config/test/generated/svelte-final-config.js
+++ packages/eslint-config/test/generated/svelte-final-config.js
@@ -3467,9 +3467,9 @@ export default [
         "parser": {
           "meta": {
             "name": "typescript-eslint/parser",
-            "version": "8.52.0"
+            "version": "8.53.0"
           },
-          "version": "8.52.0"
+          "version": "8.53.0"
         },
         "projectService": true,
         "warnOnUnsupportedTypeScriptVersion": false
@@ -4697,7 +4697,7 @@ export default [
         "parser": {
           "meta": {
             "name": "typescript-eslint/parser",
-            "version": "8.52.0"
+            "version": "8.53.0"
           }
         }
       }
